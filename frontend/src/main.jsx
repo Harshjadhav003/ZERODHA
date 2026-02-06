4f018f0b494bc4fcb70f4c5d0b1955322ac475a6
Harshvardhan jadhav <jadhavharsh6704@gmail.com>
2026-02-06 22:07:30 +0530

    Setup project folder Structrue

---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  <div>
+    <h1>Hellow</h1>
+  </div>
+  </StrictMode>,
+)
