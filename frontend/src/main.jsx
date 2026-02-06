65f24e96d7bc5e2e9bd231c53dedab5a6164a8d8
Harshvardhan jadhav <jadhavharsh6704@gmail.com>
2026-02-06 23:39:08 +0530

    Componet ReStructure Setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import HomePage from './landing_page/home/HomePage'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <div>
-    <h1>Hellow</h1>
-  </div>
+  
+    <HomePage/>
+  
   </StrictMode>,
 )
